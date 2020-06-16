7d6063164f0165b1e71b8efd3cc07af66d034911
Devon <devon.hugh.godfrey@gmail.com>
2020-06-15 20:18:29 -0400

    Broke out detail and list into two components.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 
+import { HEROES } from "../mock-heroes";
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -12,6 +14,12 @@ export class HeroesComponent implements OnInit {
     id: 1
   };
 
+  heroes = HEROES; 
+
+  selectedHero: Hero; 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() { }
 
