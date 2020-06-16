60a4a705ec5ff8ff483027bcdb6f5ebf52b710eb
Devon <devon.hugh.godfrey@gmail.com>
2020-06-15 20:36:21 -0400

    Left off on observable data

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 
-import { HEROES } from "../mock-heroes";
+//import { HEROES } from "../mock-heroes";
+import { HeroService } from "../hero.service";
+
 
 @Component({
   selector: 'app-heroes',
@@ -9,21 +11,26 @@ import { HEROES } from "../mock-heroes";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  constructor(private heroService: HeroService) { }
+  
   hero: Hero = {
     name: "Windstorm",
     id: 1
   };
 
-  heroes = HEROES; 
+  heroes: Hero[]; 
 
   selectedHero: Hero; 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
